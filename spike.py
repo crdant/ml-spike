973783464fa3551c21f1d31e9cc561283556df1c
Chuck D'Antonio <c_dantonio@acm.org>
2016-12-15 08:57:02 -0500

    Cleaned up initialization and added storage

--- spike.py
+++ spike.py
@@ -2,13 +2,20 @@
 
 from oauth2client.client import GoogleCredentials
 from oauth2client.service_account import ServiceAccountCredentials
+from oauth2client.crypt import RsaSigner
+
 from googleapiclient import discovery
 from googleapiclient import errors
+from googleapiclient.http import MediaInMemoryUpload
+from googleapiclient.http import MediaIoBaseDownload
+
 from flask import Flask, request
 from flask_restful import Resource, Api
 
 import os
 import json
+import base64
+import StringIO
 
 port = 8003
 if 'PORT' in os.environ:
@@ -19,33 +26,50 @@ Flask.get = lambda self, path: self.route(path, methods=['get'])
 app = Flask(__name__)
 api = Api(app)
 
-# Make the call.
+credentials=None
+ml=None
+storage=None
+storageBinding=None
 
+# Store your full project ID in a variable in the format the API needs.
+projectID = 'projects/{}'.format(os.environ['GCP_PROJECT'])
+services = json.loads(os.environ['VCAP_SERVICES'])
+
+if 'google-ml-apis' in services:
+    for service in services['google-ml-apis']:
+            if service.get('name') == 'prediction-apis':
+                # authenticate
+                mlBinding = service.get('credentials')
+                service_account = json.loads(base64.b64decode(mlBinding.get('PrivateKeyData')))
+                credentials = ServiceAccountCredentials.from_json_keyfile_dict(service_account)
+                # Build a representation of the Cloud ML API.
+                ml = discovery.build('ml', 'v1beta1', credentials=credentials)
+else:
+    raise EnvironmentError(1,"Learning service not bound in environment")
+
+if 'google-storage' in services:
+    for service in services['google-storage']:
+            if service.get('name') == 'model-storage':
+                # authenticate
+                storageBinding = service.get('credentials')
+                service_account = json.loads(base64.b64decode(storageBinding.get('PrivateKeyData')))
+                credentials = ServiceAccountCredentials.from_json_keyfile_dict(service_account)
+                # Build a representation of the Cloud Storage API.
+                storage = discovery.build('storage', 'v1', credentials=credentials)
+else:
+    raise EnvironmentError(1,"Learning service not bound in environment")
+
+# Make the call.
 class GoogleMachineLearningModel(Resource):
-    def __init__(self):
-        # Store your full project ID in a variable in the format the API needs.
-        projectID = 'projects/{}'.format(os.environ['GCP_PROJECT'])
-        services = json.loads(os.environ['VCAP_SERVICES'])
-        if 'google-ml-apis' in services:
-            for service in services['google-ml-apis']:
-                    if s.get('name') == 'prediction-service'
-                        # authenticate
-                        binding = s.get('credentials')
-                        credentials = ServiceAccountCredentials(binding.get('service_account_email'), NEED SIGNER,
-                            binding.get('private_key_id'), binding.get('client_id')
-                        # Build a representation of the Cloud ML API.
-                        ml = discovery.build('ml', 'v1beta1', credentials=credentials)
-        else:
-            raise EnvironmentError(1,"Service not bound in environment")
     def put(self, name):
         try:
             # Create a dictionary with the fields from the request body.
             requestDict = {'name': name,
                 'description': 'a spike to experiment with connecting to the ML APIs'}
-
+            print request.args
             # Create a request to call projects.models.create.
             request = ml.projects().models().create(
-            parent=projectID, body=requestDict)
+                parent=projectID, body=requestDict)
             response = request.execute()
             return response
         except errors.HttpError, err:
@@ -77,7 +101,48 @@ class GoogleMachineLearningModel(Resource):
             # Something went wrong, print out some information.
             return 'There was an error creating the model. Check the details:' + err._get_reason()
 
+class GoogleStorageObject(Resource):
+    def __init__(self):
+        self.bucketName=storageBinding.get('bucket_name')
+    def put(self, name):
+        try:
+            # Create a dictionary with the fields from the request body.
+            requestBody = {'name': name }
+            objectBody = MediaInMemoryUpload("hello world", mimetype="text/plain")
+            request = storage.objects().insert(bucket=self.bucketName, media_body=objectBody, body=requestBody)
+            response = request.execute()
+            return response
+        except errors.HttpError, err:
+            # Something went wrong, print out some information.
+            return 'There was an error creating the object. Check the details:' + err._get_reason()
+
+    def delete(self, name):
+        try:
+            request = storage.objects().delete(bucket=self.bucketName, object=name)
+            response = request.execute()
+            return response
+        except errors.HttpError, err:
+            # Something went wrong, print out some information.
+            return 'There was an error deleting the object. Check the details:' + err._get_reason()
+
+    def get(self, name):
+        try:
+            request = storage.objects().get_media(bucket=self.bucketName, object=name)
+            responseBuffer = StringIO.StringIO()
+            content = MediaIoBaseDownload(responseBuffer, request)
+            download_complete = False
+            error_counter = 0
+
+            while not download_complete:
+                    progress, download_complete = content.next_chunk()
+
+            return responseBuffer.getvalue()
+        except errors.HttpError, err:
+            # Something went wrong, print out some information.
+            return 'There was an error getting the object. Check the details:' + err._get_reason()
+
 api.add_resource(GoogleMachineLearningModel, '/model/<string:name>')
+api.add_resource(GoogleStorageObject, '/object/<string:name>')
 
 @app.get('/')
 def list():
@@ -87,12 +152,14 @@ def list():
         request = ml.projects().models().list( parent=projectID )
         response = request.execute()
         if response:
-            models = "<ul>"
+            models = "<h4>Models</h4><ul>"
             for i, v in enumerate(response['models']):
                models = models + v['name'] + "&nbsp;&nbsp;&nbsp;" + v['description'] + '<br> '
             models = models + "</ul>"
-            return "Models available: " + models
-        return "No models"
+        else:
+            models = "<h4>No models</h4><br/>"
+        return models
+
     except errors.HttpError, err:
         # Something went wrong, print out some information.
         return 'There was an error creating the model. Check the details:' + err._get_reason()
